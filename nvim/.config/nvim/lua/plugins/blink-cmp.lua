304ca8bd01917f67181d1474899cf5c149bbc783
Jun Kai Zhang <junkaizhang8@gmail.com>
2025-04-27 18:41:05 -0400

    Add custom Karabiner Elements keybindings

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -1,6 +1,8 @@
 return {
   "saghen/blink.cmp",
+  event = { "InsertEnter", "CmdlineEnter" },
   opts = function(_, opts)
     opts.keymap = { preset = "default" }
+    opts.cmdline = { enabled = true }
   end,
 }
