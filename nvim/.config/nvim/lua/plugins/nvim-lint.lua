a9cd6ef23041b928eda8eca6f12bde86499d710b
Jun Kai Zhang <junkaizhang8@gmail.com>
2025-07-12 20:34:44 -0400

    Disable line length warnings for markdown files

---
+++ nvim/.config/nvim/lua/plugins/nvim-lint.lua
@@ -0,0 +1,12 @@
+local HOME = os.getenv("HOME")
+return {
+  "mfussenegger/nvim-lint",
+  optional = true,
+  opts = {
+    linters = {
+      ["markdownlint-cli2"] = {
+        args = { "--config", vim.fn.stdpath("config") .. "/linter_config/global.markdown-cli2.jsonc", "--" },
+      },
+    },
+  },
+}
