d0043d40d0fdf59bf54afe49a4834a538427d533
Jun Kai Zhang <junkaizhang8@gmail.com>
2025-07-12 20:37:10 -0400

    Minor updates to Snacks.nvim and add new keymap for toggling Copilot

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -1,20 +1,44 @@
 return {
   "folke/snacks.nvim",
   opts = function(_, opts)
+    opts.scroll = {
+      enabled = true,
+      animate = {
+        duration = { step = 10, total = 80 },
+        easing = "linear",
+      },
+      animate_repeat = {
+        delay = 50,
+        duration = { step = 3, total = 20 },
+        easing = "linear",
+      },
+    }
+    opts.styles = {
+      snacks_image = {
+        relative = "editor",
+        col = -1,
+      },
+    }
     opts.image = {
       enabled = true,
       doc = {
         inline = false,
         float = true,
-        max_width = 80,
-        max_height = 40,
+        max_width = 60,
+        max_height = 30,
       },
     }
     opts.notifier = {
       enabled = true,
       filter = function(notif)
+        local msg = notif.msg
         -- Filter out notification from img-clip when pasting
-        if notif.msg == "Content is not an image." then
+        if msg == "Content is not an image." then
+          return false
+        end
+        -- Disable bugged notification that shows in markdown files when
+        -- you press <enter> when ghost text is showing
+        if msg:match("ghost_text/utils.lua:%d+: Invalid buffer id") then
           return false
         end
         return true
@@ -32,5 +56,28 @@ return {
         },
       },
     }
+
+    local Snacks = require("snacks")
+    local copilot_exists = pcall(require, "copilot")
+
+    if copilot_exists then
+      Snacks.toggle({
+        name = "Copilot Completion",
+        color = {
+          enabled = "azure",
+          disabled = "orange",
+        },
+        get = function()
+          return not require("copilot.client").is_disabled()
+        end,
+        set = function(state)
+          if state then
+            require("copilot.command").enable()
+          else
+            require("copilot.command").disable()
+          end
+        end,
+      }):map("<leader>aT")
+    end
   end,
 }
