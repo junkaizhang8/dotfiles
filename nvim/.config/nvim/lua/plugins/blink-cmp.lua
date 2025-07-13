7a27b34b8b518bb588a7c4e0b891d12eade36685
Jun Kai Zhang <junkaizhang8@gmail.com>
2025-07-12 20:31:38 -0400

    Improve styling for hover documentation and signatures

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -4,5 +4,10 @@ return {
   opts = function(_, opts)
     opts.keymap = { preset = "default" }
     opts.cmdline = { enabled = true }
+    opts.completion = {
+      menu = { border = "rounded" },
+      documentation = { window = { border = "rounded" } },
+    }
+    opts.signature = { window = { border = "rounded" } }
   end,
 }
---
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,31 @@
+return {
+  "folke/noice.nvim",
+  opts = {
+    lsp = {
+      override = {
+        -- override the default lsp markdown formatter with Noice
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = false,
+        -- override the lsp markdown formatter with Noice
+        ["vim.lsp.util.stylize_markdown"] = false,
+        -- override cmp documentation with Noice (needs the other options to work)
+        ["cmp.entry.get_documentation"] = false,
+      },
+      documentation = {
+        opts = {
+          lang = "markdown",
+          replace = true,
+          render = "plaintext",
+          format = { "{message}" },
+          win_options = { concealcursor = "n", conceallevel = 3 },
+          size = { max_height = 15 },
+        },
+      },
+      signature = {
+        opts = { size = { max_height = 15 } },
+      },
+    },
+    presets = {
+      lsp_doc_border = true,
+    },
+  },
+}
