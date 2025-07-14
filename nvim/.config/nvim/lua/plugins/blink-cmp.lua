3ea58cb9d3bda00f6d359d54af37cc8b906c555b
Jun Kai Zhang <junkaizhang8@gmail.com>
2025-07-14 12:58:57 -0400

    Add word completion from a dictionary

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -1,13 +1,35 @@
 return {
   "saghen/blink.cmp",
+  dependencies = {
+    {
+      "Kaiser-Yang/blink-cmp-dictionary",
+      dependencies = { "nvim-lua/plenary.nvim" },
+    },
+  },
   event = { "InsertEnter", "CmdlineEnter" },
-  opts = function(_, opts)
-    opts.keymap = { preset = "default" }
-    opts.cmdline = { enabled = true }
-    opts.completion = {
+  opts = {
+    completion = {
       menu = { border = "rounded" },
       documentation = { window = { border = "rounded" } },
-    }
-    opts.signature = { window = { border = "rounded" } }
-  end,
+    },
+    signature = { window = { border = "rounded" } },
+    sources = {
+      default = { "dictionary" },
+      providers = {
+        dictionary = {
+          name = "Dict",
+          module = "blink-cmp-dictionary",
+          max_items = 8,
+          min_keyword_length = 3,
+          -- Make sure it is last in priority so it does not conflict with other sources
+          score_offset = -100,
+          opts = {
+            -- Do not specify a file, just the path, and in the path you need to
+            -- have your .txt files
+            dictionary_directories = { vim.fn.expand("~/.dotfiles/dictionaries") },
+          },
+        },
+      },
+    },
+  },
 }
