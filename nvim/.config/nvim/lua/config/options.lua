f54388b77d240392eced023df0c60f4ec6f5393b
Jun Kai Zhang <junkaizhang8@gmail.com>
2025-08-08 01:50:02 -0400

    Add VimTeX support

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -8,3 +8,6 @@ vim.cmd([[let &t_Ce = "\e[4:0m"]])
 
 -- Disable all animations
 vim.g.snacks_animate = false
+
+-- Don't open Quickfix for warnings in VimTeX
+vim.g.vimtex_quickfix_open_on_warning = 0
