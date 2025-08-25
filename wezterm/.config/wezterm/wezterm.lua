5e966e214048618db8498bca5ea093d6c0e193ce
Jun Kai Zhang <junkaizhang8@gmail.com>
2025-08-25 19:19:29 -0400

    Update WezTerm config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
-config.font_size = 19
+config.font_size = 16
 
 config.hide_tab_bar_if_only_one_tab = true
 
@@ -68,6 +68,6 @@ config.mouse_bindings = {
 }
 
 config.window_background_opacity = 0.75
-config.macos_window_background_blur = 10
+config.macos_window_background_blur = 40
 
 return config
