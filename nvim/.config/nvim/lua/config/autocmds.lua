999704e3d2e76b0356556e7af7a1ee258fcbd985
Jun Kai Zhang <junkaizhang8@gmail.com>
2025-08-25 00:40:41 -0400

    Move lualine autocmd outside of config

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -12,3 +12,49 @@ vim.api.nvim_create_autocmd("InsertLeave", {
   pattern = "*",
   command = "set nopaste",
 })
+
+vim.api.nvim_create_autocmd({ "BufEnter", "WinEnter", "TermLeave", "BufWinEnter" }, {
+  callback = function()
+    local dashboard_filetypes = {
+      alpha = true,
+      dashboard = true,
+      snacks_dashboard = true,
+    }
+
+    local function is_dashboard()
+      return dashboard_filetypes[vim.bo.filetype] == true
+    end
+
+    local function get_lualine_colors()
+      local lualine = require("lualine")
+      local theme = lualine.get_config().options.theme
+
+      if type(theme) == "string" then
+        theme = require("lualine.themes." .. theme)
+      end
+
+      return theme.normal.c
+    end
+
+    local function set_statusline_highlight()
+      local colors = get_lualine_colors()
+      vim.api.nvim_set_hl(0, "StatusLine", { bg = colors.bg, fg = colors.fg })
+    end
+
+    local function hide_statusline_highlight()
+      vim.api.nvim_set_hl(0, "StatusLine", { bg = "none", fg = "none" })
+    end
+
+    local function update_lualine_statusline()
+      if is_dashboard() then
+        -- Hide lualine when entering dashboard
+        hide_statusline_highlight()
+      else
+        -- Call this to fix weird coloring artifacts in the status line
+        set_statusline_highlight()
+      end
+    end
+
+    vim.schedule(update_lualine_statusline)
+  end,
+})
