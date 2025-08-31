9370c4c4ec9c966a0a11399a200961b5c0cc19c9
Jun Kai Zhang <junkaizhang8@gmail.com>
2025-08-30 20:09:03 -0400

    Update avante.nvim config

--- nvim/.config/nvim/lua/plugins/avante.lua
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -2,29 +2,6 @@ return {
   "yetone/avante.nvim",
   event = "VeryLazy",
   version = false, -- Never set this value to "*"! Never!
-  opts = {
-    -- add any opts here
-    -- for example
-    provider = "copilot",
-    providers = {
-      copilot = {
-        model = "claude-3.7-sonnet",
-      },
-      behaviour = {
-        auto_suggestions = false,
-      },
-      openai = {
-        endpoint = "https://api.githubcopilot.com",
-        model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-        timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-        extra_request_body = {
-          temperature = 0,
-          max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-          reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
-        },
-      },
-    },
-  },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
@@ -41,16 +18,49 @@ return {
     "nvim-tree/nvim-web-devicons", -- or nvim-mini/mini.icons
     "zbirenbaum/copilot.lua", -- for providers='copilot'
     "HakonHarnes/img-clip.nvim", -- for image upload support
-    {
-      -- Make sure to set this up properly if you have lazy=true
-      "MeanderingProgrammer/render-markdown.nvim",
-      opts = {
-        file_types = { "markdown", "Avante" },
-      },
-      ft = { "markdown", "Avante" },
-    },
+    "MeanderingProgrammer/render-markdown.nvim", -- for markdown rendering
   },
   keys = {
-    { "<leader>ax", "<cmd>AvanteClear<cr>", desc = "avante: clear" },
+    { "<leader>ax", "<Cmd>AvanteClear<CR>", desc = "avante: clear" },
   },
+  opts = {
+    instruction_file = "avante.md",
+    provider = "copilot",
+    providers = {
+      copilot = {
+        model = "claude-3.7-sonnet",
+        timeout = 30000,
+        extra_request_body = {
+          temperature = 0.75,
+          max_tokens = 8192,
+        },
+      },
+      openai = {
+        endpoint = "https://api.githubcopilot.com",
+        model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
+        timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+        extra_request_body = {
+          temperature = 0,
+          max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+          reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+        },
+      },
+    },
+    behaviour = {
+      auto_suggestions = false,
+    },
+  },
+  config = function(_, opts)
+    require("avante").setup(opts)
+
+    local wk = require("which-key")
+
+    if not wk then
+      return
+    end
+
+    wk.add({
+      { "<leader>a", group = "+avante/ai", icon = "", mode = { "n", "v" } },
+    })
+  end,
 }
