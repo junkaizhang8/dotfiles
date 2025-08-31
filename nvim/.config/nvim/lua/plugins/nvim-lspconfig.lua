080a9d167d01df5ba4b2bfea4bc41dd10e0ef467
Jun Kai Zhang <junkaizhang8@gmail.com>
2025-08-31 15:42:07 -0400

    Add keymap for restarting LSP

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,22 +1,30 @@
 return {
   "neovim/nvim-lspconfig",
-  opts = {
-    diagnostics = {
-      float = {
-        border = "rounded",
-      },
-    },
-  },
-  init = function()
+  opts = function(_, opts)
+    local keys = require("lazyvim.plugins.lsp.keymaps").get()
+
     -- List of keymaps to disable
     local disabledKeys = {
       "<leader>ca",
     }
 
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
+    -- List of keymaps to add
+    local newKeys = {
+      { "<leader>cL", "<Cmd>LspRestart<CR>", desc = "Restart LSP" },
+    }
 
     for _, key in ipairs(disabledKeys) do
       keys[#keys + 1] = { key, false }
     end
+
+    for _, mapping in ipairs(newKeys) do
+      keys[#keys + 1] = mapping
+    end
+
+    opts.diagnostics = {
+      float = {
+        border = "rounded",
+      },
+    }
   end,
 }
