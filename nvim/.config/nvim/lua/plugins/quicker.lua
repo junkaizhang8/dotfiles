6fc99d5c135454c0a82bfad0a506196be12a43b0
Jun Kai Zhang <junkaizhang8@gmail.com>
2025-10-06 16:55:24 -0400

    feat(nvim): add quicker.nvim for improved quickfix UI

---
+++ nvim/.config/nvim/lua/plugins/quicker.lua
@@ -0,0 +1,45 @@
+return {
+  "stevearc/quicker.nvim",
+  event = "VeryLazy",
+  keys = {
+    {
+      "<leader>xf",
+      function()
+        require("quicker").toggle()
+      end,
+      desc = "Quickfix List",
+    },
+    {
+      "<leader>xl",
+      function()
+        require("quicker").toggle({ loclist = true })
+      end,
+      desc = "Location List",
+    },
+  },
+  opts = {
+    keys = {
+      {
+        "<leader>>",
+        function()
+          require("quicker").expand({ before = 2, after = 2, add_to_existing = true })
+        end,
+        desc = "Expand Context",
+      },
+      {
+        "<leader><",
+        function()
+          require("quicker").collapse()
+        end,
+        desc = "Collapse Context",
+      },
+    },
+  },
+  config = function(_, opts)
+    require("quicker").setup(opts)
+
+    local normal_fg = vim.api.nvim_get_hl(0, { name = "Normal" }).fg
+
+    vim.api.nvim_set_hl(0, "QuickFixLineNr", { fg = normal_fg })
+  end,
+}
