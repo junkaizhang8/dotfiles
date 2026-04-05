e53989d26088aac5d3ce20af653be457cc897876
Jun Kai Zhang <junkaizhang8@gmail.com>
2026-04-04 22:37:12 -0400

    feat(nvim): remove pyenv support for venv-selector.nvim

--- nvim/.config/nvim/lua/plugins/venv-selector.lua
+++ nvim/.config/nvim/lua/plugins/venv-selector.lua
@@ -1,13 +1,4 @@
 return {
   "linux-cultist/venv-selector.nvim",
   branch = "main", -- VenvSelect is using `main` as the updated branch again
-  opts = function()
-    return {
-      search = {
-        pyenv = {
-          command = "fd '^python$' $(pyenv root)/versions -t l",
-        },
-      },
-    }
-  end,
 }
