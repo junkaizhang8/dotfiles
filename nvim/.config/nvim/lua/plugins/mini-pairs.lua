37d9b3d4f36e2c667482ad723191e590a4689d69
Jun Kai Zhang <junkaizhang8@gmail.com>
2026-04-06 00:52:02 -0400

    feat(nvim): replace mini.pairs with ultimate-autopair

--- nvim/.config/nvim/lua/plugins/mini-pairs.lua
+++ nvim/.config/nvim/lua/plugins/mini-pairs.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-mini/mini.pairs",
+  enabled = false,
   opts = {
     -- Disable in command mode because it can get annoying
     modes = { insert = true, command = false, terminal = false },
