7cff08b00a27f154bf2b9a79adcf5e3cac8717bc
Jun Kai Zhang <junkaizhang8@gmail.com>
2026-04-08 17:27:31 -0400

    feat(nvim): begin migration to personal config

---
+++ nvim-new/.config/nvim-new/init.lua
@@ -0,0 +1,40 @@
+-- Install lazy.nvim if not already installed
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.uv.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+---@type LazySpec
+local plugins = "plugins"
+
+require("config.options")
+require("config.keymaps")
+require("config.autocmds")
+
+require("lazy").setup(plugins, {
+  ui = { border = "rounded" },
+  change_detection = { notify = false },
+  rocks = {
+    enabled = false,
+  },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
---
+++ nvim-new/.config/nvim-new/lua/config/icons.lua
@@ -0,0 +1,87 @@
+local M = {}
+
+M.dap = {
+  stopped = { "󰁕", "DiagnosticWarn", "DapStoppedLine" },
+  breakpoint = "",
+  breakpoint_condition = "",
+  breakpoint_rejected = { "", "DiagnosticError" },
+  log_point = ".>",
+}
+
+M.folding = {
+  open = "",
+  close = "",
+}
+
+M.diagnostics = {
+  error = "",
+  warn = "",
+  hint = "",
+  info = "",
+}
+
+M.git = {
+  added = "",
+  modified = "",
+  removed = "",
+}
+
+M.kinds = {
+  Array = "",
+  Boolean = "󰨙",
+  Class = "",
+  Codeium = "󰘦",
+  Color = "",
+  Control = "",
+  Collapsed = "",
+  Constant = "󰏿",
+  Constructor = "",
+  Copilot = "",
+  Enum = "",
+  EnumMember = "",
+  Event = "",
+  Field = "",
+  File = "",
+  Folder = "",
+  Function = "󰊕",
+  Interface = "",
+  Key = "",
+  Keyword = "",
+  Method = "󰊕",
+  Module = "",
+  Namespace = "󰦮",
+  Null = "",
+  Number = "󰎠",
+  Object = "",
+  Operator = "",
+  Package = "",
+  Property = "",
+  Reference = "",
+  Snippet = "󱄽",
+  String = "",
+  Struct = "󰆼",
+  Supermaven = "",
+  TabNine = "󰏚",
+  Text = "",
+  TypeParameter = "",
+  Unit = "",
+  Value = "",
+  Variable = "󰀫",
+}
+
+-- Miscellaneous icons that don't fit into the above categories
+M.misc = {
+  bug = "",
+  dashed_bar = "┊",
+  vertical_bar = "│",
+  ellipsis = "…",
+  git = "",
+  gh = "",
+  palette = "󰏘",
+  robot = "󰚩",
+  search = "",
+  terminal = "",
+  toolbox = "󰦬",
+}
+
+return M
