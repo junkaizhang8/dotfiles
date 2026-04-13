5aef6938f3496c2630d93733fd3255e2f46b1521
Jun Kai Zhang <junkaizhang8@gmail.com>
2026-04-13 01:07:05 -0400

    feat(nvim): update LSP diagnostic sign icons

--- nvim-new/.config/nvim-new/lua/config/lsp.lua
+++ nvim-new/.config/nvim-new/lua/config/lsp.lua
@@ -1,6 +1,24 @@
+local icons = require("config.icons")
+
+local config = {
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = icons.diagnostics.error .. " ",
+      [vim.diagnostic.severity.WARN] = icons.diagnostics.warn .. " ",
+      [vim.diagnostic.severity.INFO] = icons.diagnostics.info .. " ",
+      [vim.diagnostic.severity.HINT] = icons.diagnostics.hint .. " ",
+    },
+  },
+}
+
+vim.diagnostic.config(config)
+
 vim.api.nvim_create_autocmd({ "BufReadPre", "BufNewFile" }, {
   once = true,
   callback = function()
+    -- Extend Neovim's client capabilities with the completion ones
+    vim.lsp.config("*", { capabilities = require("blink.cmp").get_lsp_capabilities(nil, true) })
+
     local servers = vim
       .iter(vim.api.nvim_get_runtime_file("lsp/*.lua", true))
       :map(function(file)
