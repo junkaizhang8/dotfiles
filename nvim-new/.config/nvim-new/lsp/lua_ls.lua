39be612e2eec38a59946adc6192d1f3f207512f6
Jun Kai Zhang <junkaizhang8@gmail.com>
2026-04-13 23:41:18 -0400

    feat(nvim): order all Lua project root markers under the same priority

--- nvim-new/.config/nvim-new/lsp/lua_ls.lua
+++ nvim-new/.config/nvim-new/lsp/lua_ls.lua
@@ -4,8 +4,7 @@
 local utils = require("utils.root")
 
 local root_markers = {
-  { ".luarc.json", ".luarc.jsonc", ".luacheckrc" },
-  { ".stylua.toml", "stylua.toml", "selene.toml", "selene.yml" },
+  { ".luarc.json", ".luarc.jsonc", ".luacheckrc", ".stylua.toml", "stylua.toml", "selene.toml", "selene.yml" },
   ".git",
 }
 
