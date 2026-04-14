d287c6243f6b1b0fb921a35afc4d0e3311b219d6
Jun Kai Zhang <junkaizhang8@gmail.com>
2026-04-13 22:54:43 -0400

    feat(nvim): add `nvim-lint`

---
+++ nvim-new/.config/nvim-new/lua/plugins/nvim-lint.lua
@@ -0,0 +1,21 @@
+return {
+  "mfussenegger/nvim-lint",
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    linters_by_ft = {
+      javascript = { "eslint_d" },
+      javascriptreact = { "eslint_d" },
+      typescript = { "eslint_d" },
+      typescriptreact = { "eslint_d" },
+    },
+  },
+  config = function()
+    local lint = require("lint")
+    vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
+      group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
+      callback = function()
+        lint.try_lint()
+      end,
+    })
+  end,
+}
