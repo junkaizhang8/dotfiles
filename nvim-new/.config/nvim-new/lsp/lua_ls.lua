c476485de37b8444c60917eb2a5546498780865e
Jun Kai Zhang <junkaizhang8@gmail.com>
2026-04-13 22:55:54 -0400

    feat(nvim): add extra `lua_ls` root markers

--- nvim-new/.config/nvim-new/lsp/lua_ls.lua
+++ nvim-new/.config/nvim-new/lsp/lua_ls.lua
@@ -4,11 +4,9 @@
 local utils = require("utils.root")
 
 local root_markers = {
-  ".luarc.json",
-  ".luarc.jsonc",
-  ".luacheckrc",
-  ".stylua.toml",
-  "stylua.toml",
+  { ".luarc.json", ".luarc.jsonc", ".luacheckrc" },
+  { ".stylua.toml", "stylua.toml", "selene.toml", "selene.yml" },
+  ".git",
 }
 
 ---@type vim.lsp.Config
