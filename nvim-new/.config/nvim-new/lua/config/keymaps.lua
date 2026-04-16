74d6450e60904c256e37052c7c7e8855a90c1bef
Jun Kai Zhang <junkaizhang8@gmail.com>
2026-04-16 19:15:14 -0400

    feat(nvim): enable treesitter folding

--- nvim-new/.config/nvim-new/lua/config/keymaps.lua
+++ nvim-new/.config/nvim-new/lua/config/keymaps.lua
@@ -61,6 +61,7 @@ local diagnostic_goto = function(next, severity)
   end
 end
 
+-- Diagnostics
 map("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
 map("n", "]d", diagnostic_goto(true), { desc = "Next Diagnostic" })
 map("n", "[d", diagnostic_goto(false), { desc = "Prev Diagnostic" })
