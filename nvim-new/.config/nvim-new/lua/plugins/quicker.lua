dadf30ceeb9d564baf9ec0acf6a01de63ded9c52
Jun Kai Zhang <junkaizhang8@gmail.com>
2026-04-16 19:58:48 -0400

    feat(nvim): add `quicker.nvim`

---
+++ nvim-new/.config/nvim-new/lua/plugins/quicker.lua
@@ -0,0 +1,39 @@
+return {
+  -- TODO: abc
+  "stevearc/quicker.nvim",
+  event = "VeryLazy",
+  keys = {
+    {
+      "<leader>xq",
+      function()
+        require("quicker").toggle()
+      end,
+      desc = "Quickfix List",
+    },
+    {
+      "<leader>xl",
+      function()
+        require("quicker").toggle({ loclist = true })
+      end,
+      desc = "Location List",
+    },
+  },
+  opts = {
+    keys = {
+      {
+        "<leader>>",
+        function()
+          require("quicker").expand({ before = 2, after = 2, add_to_existing = true })
+        end,
+        desc = "Expand Context",
+      },
+      {
+        "<leader><",
+        function()
+          require("quicker").collapse()
+        end,
+        desc = "Collapse Context",
+      },
+    },
+  },
+}
