007c23a4dc50de447b4d123d9a0c1f633d4e8dd5
Jun Kai Zhang <junkaizhang8@gmail.com>
2026-04-17 00:30:34 -0400

    feat(nvim): add C language server

---
+++ nvim-new/.config/nvim-new/lsp/clangd.lua
@@ -0,0 +1,25 @@
+-- Install with:
+-- Mac: brew install llvm
+
+local utils = require("utils.root")
+
+local root_markers = {
+  { ".clangd", ".clang-tidy", ".clang-format", "compile_commands.json", "compile_flags.txt", "configure.ac" },
+  ".git",
+}
+
+---@type vim.lsp.Config
+return {
+  cmd = {
+    "clangd",
+    "--clang-tidy",
+    "--header-insertion=iwyu",
+    "--completion-style=detailed",
+    "--fallback-style=none",
+    "--function-arg-placeholders=false",
+  },
+  filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
+  root_dir = function(bufnr, on_dir)
+    on_dir(utils.lspconfig.root_pattern(bufnr, root_markers))
+  end,
+}
