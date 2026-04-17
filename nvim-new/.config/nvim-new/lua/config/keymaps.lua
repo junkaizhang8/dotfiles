6f18509a9e6e6a1cc596fa12fd99b3d89553bef4
Jun Kai Zhang <junkaizhang8@gmail.com>
2026-04-16 21:11:01 -0400

    feat(nvim): reimplement `Snacks.rename.rename_file` into its own module

--- nvim-new/.config/nvim-new/lua/config/keymaps.lua
+++ nvim-new/.config/nvim-new/lua/config/keymaps.lua
@@ -50,25 +50,9 @@ map({ "i", "s", "n" }, "<Esc>", function()
   return "<Esc>"
 end, { desc = "Escape, Clear hlsearch, and Stop Snippet Session", expr = true })
 
--- LSP
-local diagnostic_goto = function(next, severity)
-  return function()
-    vim.diagnostic.jump({
-      count = (next and 1 or -1) * vim.v.count1,
-      severity = severity and vim.diagnostic.severity[severity] or nil,
-      float = true,
-    })
-  end
-end
-
--- Diagnostics
-map("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
-map("n", "]d", diagnostic_goto(true), { desc = "Next Diagnostic" })
-map("n", "[d", diagnostic_goto(false), { desc = "Prev Diagnostic" })
-map("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
-map("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
-map("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
-map("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
+map("n", "<leader>cR", function()
+  require("modules.rename-file").rename_file()
+end, { desc = "Rename File" })
 
 -- Go to the start of the line while in insert mode
 map({ "i", "c" }, "<C-h>", "<C-o>I", { desc = "Go to Start of Line" })
