688b3b29f4a681d76d2b932df628368715f7ed17
Jun Kai Zhang <junkaizhang8@gmail.com>
2026-04-18 15:20:29 -0400

    feat(nvim): add user command to check linter information

--- nvim-new/.config/nvim-new/lua/plugins/nvim-lint.lua
+++ nvim-new/.config/nvim-new/lua/plugins/nvim-lint.lua
@@ -3,11 +3,7 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   opts = {
     linters_by_ft = {
-      javascript = { "eslint_d" },
-      javascriptreact = { "eslint_d" },
       markdown = { "markdownlint-cli2" },
-      typescript = { "eslint_d" },
-      typescriptreact = { "eslint_d" },
     },
   },
   config = function(_, opts)
@@ -21,5 +17,17 @@ return {
         lint.try_lint()
       end,
     })
+
+    -- https://github.com/mfussenegger/nvim-lint/issues/559#issuecomment-2263995711
+    vim.api.nvim_create_user_command("LintInfo", function()
+      local filetype = vim.bo.filetype
+      local linters = require("lint").linters_by_ft[filetype]
+
+      if linters then
+        print("Linters for " .. filetype .. ": " .. table.concat(linters, ", "))
+      else
+        print("No linters configured for filetype: " .. filetype)
+      end
+    end, {})
   end,
 }
