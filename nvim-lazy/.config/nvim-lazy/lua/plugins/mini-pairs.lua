5de65a05edaea77260902a1977422e82157cdd32
Jun Kai Zhang <junkaizhang8@gmail.com>
2026-04-18 20:36:23 -0400

    feat(nvim-lazy): remove `ultimate-autopair.nvim`

--- nvim-lazy/.config/nvim-lazy/lua/plugins/mini-pairs.lua
+++ nvim-lazy/.config/nvim-lazy/lua/plugins/mini-pairs.lua
@@ -1,6 +1,5 @@
 return {
   "nvim-mini/mini.pairs",
-  enabled = true,
   opts = {
     -- Disable in command mode because it can get annoying
     modes = { insert = true, command = false, terminal = false },
--- nvim-lazy/.config/nvim-lazy/lua/plugins/ultimate-autopair.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "altermo/ultimate-autopair.nvim",
-  enabled = false,
-  event = { "InsertEnter", "CmdlineEnter" },
-  branch = "v0.6",
-}
