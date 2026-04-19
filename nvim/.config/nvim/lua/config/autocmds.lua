43a76defce21302ee8ca025fbae59871c6d81a66
Jun Kai Zhang <junkaizhang8@gmail.com>
2026-04-19 15:54:59 -0400

    feat(nvim): add new autocommands

---
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -0,0 +1,43 @@
+local function augroup(name)
+  return vim.api.nvim_create_augroup("junkaizhang8/" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("close_with_q"),
+  pattern = {
+    "checkhealth",
+    "grug-far",
+    "help",
+    "man",
+    "qf",
+  },
+  desc = "Close with 'q'",
+  callback = function(args)
+    local bufnr = args.buf
+    vim.bo[bufnr].buflisted = false
+    vim.schedule(function()
+      vim.keymap.set("n", "q", function()
+        if args.match == "man" then
+          vim.cmd("quit")
+        else
+          vim.cmd("close")
+          pcall(vim.api.nvim_buf_delete, bufnr, { force = true })
+        end
+      end, {
+        buffer = bufnr,
+        silent = true,
+        desc = "Quit Buffer",
+      })
+    end)
+  end,
+})
+
+vim.api.nvim_create_autocmd("VimResized", {
+  group = augroup("resize_splits"),
+  desc = "Resize splits when the Vim window is resized",
+  callback = function()
+    local current_tab = vim.fn.tabpagenr()
+    vim.cmd("tabdo wincmd =")
+    vim.cmd("tabnext " .. current_tab)
+  end,
+})
