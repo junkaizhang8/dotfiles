99de2468a4f6d3217524bc6082c94f35722c280d
Jun Kai Zhang <junkaizhang8@gmail.com>
2026-04-19 15:54:40 -0400

    feat(nvim): add `grug-far.nvim`

---
+++ nvim/.config/nvim/lua/plugins/grug-far.lua
@@ -0,0 +1,21 @@
+return {
+  "MagicDuck/grug-far.nvim",
+  cmd = { "GrugFar", "GrugFarWithin" },
+  keys = {
+    {
+      "<leader>sr",
+      function()
+        local grug = require("grug-far")
+        local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+        grug.open({
+          transient = true,
+          prefills = {
+            filesFilter = ext and ext ~= "" and "*." .. ext or nil,
+          },
+        })
+      end,
+      desc = "Search and Replace",
+      mode = { "n", "x" },
+    },
+  },
+}
