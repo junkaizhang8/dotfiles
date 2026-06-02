41c0ef28e586012d8ddd378f58a19681a9c696ef
Jun Kai Zhang <junkaizhang8@gmail.com>
2026-06-01 23:19:06 -0400

    feat(nvim): add `markdown-toc`

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -21,7 +21,7 @@ return {
       json = { "prettierd" },
       jsonc = { "prettierd" },
       lua = { "stylua" },
-      markdown = { "prettierd" },
+      markdown = { "prettierd", "markdown-cli2", "markdown-toc" },
       scss = { "prettierd" },
       sh = { "shfmt" },
       typescript = { "prettierd" },
@@ -48,6 +48,23 @@ return {
       return {}
     end,
     formatters = {
+      ["markdown-toc"] = {
+        condition = function(_, ctx)
+          for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
+            if line:find("<!%-%- toc %-%->") then
+              return true
+            end
+          end
+        end,
+      },
+      ["markdownlint-cli2"] = {
+        condition = function(_, ctx)
+          local diag = vim.tbl_filter(function(d)
+            return d.source == "markdownlint"
+          end, vim.diagnostic.get(ctx.buf))
+          return #diag > 0
+        end,
+      },
       injected = { options = { ignore_errors = true } },
     },
     -- Show a notification when formatting fails
