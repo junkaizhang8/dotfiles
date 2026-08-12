a4e820735c39aa93df63594044db7aef090fe706
Jun Kai Zhang <junkaizhang8@gmail.com>
2026-08-11 20:57:44 -0400

    feat(nvim): add global config to disable line length warning in markdown files

--- nvim/.config/nvim/lua/plugins/nvim-lint.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lint.lua
@@ -12,6 +12,11 @@ return {
     -- We need to set linters_by_ft here, otherwise it uses the plugin defaults
     lint.linters_by_ft = opts.linters_by_ft
 
+    local md_config = vim.fs.joinpath(vim.fn.stdpath("config"), "linters", "markdownlint.yaml")
+    if vim.uv.fs_stat(md_config) then
+      lint.linters["markdownlint-cli2"].args = { "--config", md_config, "-" }
+    end
+
     vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
       group = vim.api.nvim_create_augroup("junkaizhang8/nvim-lint", { clear = true }),
       callback = function()
